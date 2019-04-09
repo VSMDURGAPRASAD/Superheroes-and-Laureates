220279cadb008112f1bb3e1affc8ba051cba3f0f
VSMDURGAPRASAD <sai1996.vinukonda@gmail.com>
2019-04-09 16:49:24 -0500

    Initial Commit

---
+++ Superheroes and Laureates/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Superheroes and Laureates
+//
+//  Created by Vinukonda,Sai Manikanta Durga Prasad on 4/9/19.
+//  Copyright © 2019 Vinukonda,Sai Manikanta Durga Prasad. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
